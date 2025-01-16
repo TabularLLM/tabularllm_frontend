047951d4782a74dae4d2374a68ecbdfb77a64ae6
Anthony Thanpoovong <thanpoovong84@gmail.com>
2025-01-16 00:26:47 -0500

    testing errors

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+  return (
+    <h1 style={{ fontWeight: 'bold' }}>Welcome to the Landing Page</h1>
+  );
+};
+
+export default LandingPage;
